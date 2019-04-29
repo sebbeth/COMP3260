a08b78e963c356fd9977b209ca4dce99a9c29728
Sebastian Brown <developer2025@gmail.com>
2019-04-29 23:07:15 +1000

    Because automated testing is fun, I implemented a testing class that generates input and compares result of encryption and then decryption

--- Application.java
+++ Application.java
@@ -22,6 +22,36 @@ public class Application {
     }
 
     public static void main(String[] args) {
+       
+
+        if (args.length > 0) {
+        try {
+        switch (args[0]) {
+        case "-e":
+        case "-E":
+        modeOfOperation = cryptMode.ENCRYPT;
+        break;
+        case "-d":
+        case "-D":
+        modeOfOperation = cryptMode.DECRYPT;
+        break;
+        default:
+        throw new IllegalArgumentException(
+        "No valid mode of operation was used. Please enter '-e' for encryption or '-d' for decryption.");
+        }
+
+        loadData(args[1]);
+
+        if (modeOfOperation == cryptMode.ENCRYPT) {
+        processEncryption();
+        } else if (modeOfOperation == cryptMode.DECRYPT) {
+        //processDecryption();
+        }
+        } catch (Exception e) {
+        System.out.println("Error: " + e);
+        }
+        } else {
+        //System.out.println("No file provided. Ending program.\n");
         try {
             modeOfOperation = cryptMode.ENCRYPT;
             loadData("input.txt");
@@ -29,40 +59,22 @@ public class Application {
         } catch (Exception e) {
             System.out.println(e);
         }
-
-        // if (args.length > 0) {
-        // try {
-        // switch (args[0]) {
-        // case "-e":
-        // case "-E":
-        // modeOfOperation = cryptMode.ENCRYPT;
-        // break;
-        // case "-d":
-        // case "-D":
-        // modeOfOperation = cryptMode.DECRYPT;
-        // break;
-        // default:
-        // throw new IllegalArgumentException(
-        // "No valid mode of operation was used. Please enter '-e' for encryption or
-        // '-d' for decryption.");
-        // }
-
-        // loadData(args[1]);
-
-        // if (modeOfOperation == cryptMode.ENCRYPT) {
-        // processEncryption();
-        // } else if (modeOfOperation == cryptMode.DECRYPT) {
-        // processDecryption();
-        // }
-        // } catch (Exception e) {
-        // System.out.println("Error: " + e);
-        // }
-        // } else {
-        // System.out.println("No file provided. Ending program.\n");
-        // }
+        }
     }
 
-    private static void loadData(String fileName) throws Exception {
+    public static void testLoadData(String textLine, String keyLine) throws Exception {
+
+        rawText = textLine;
+        rawKey = keyLine;
+
+        for (int i = 0; i < 16; i++) {
+            // System.out.println(i + " " + textLine.substring(i, i+8));
+            text[i] = (byte) Integer.parseInt(textLine.substring(i * 8, i * 8 + 8), 2);
+            key[i] = (byte) Integer.parseInt(keyLine.substring(i * 8, i * 8 + 8), 2);
+        }
+    }
+
+    public static void loadData(String fileName) throws Exception {
         Scanner dataFile = null;
 
         try {
@@ -90,25 +102,35 @@ public class Application {
         }
     }
 
-    private static void processEncryption() {
+    public static String[] processEncryption() {
         AES vanillaAES = new AES();
         // For each of the 128 variations of the text and key
         // Encrypt the text and key in the five variations.
         // byte[][] results = vanillaAES.encrypt(text, key);
         String[] results = vanillaAES.encrypt(rawText, rawKey);
         outputEncryptionResults(results);
+        return results;
     }
 
-    // private static void processDecryption() {
-
-    // }
+     public static String[] processDecryption() {
+         String[] output = {"TODO IMPLEMENT DECRYPT"}; // TODO Replace this
+        return output;
+     }
 
     private static void outputEncryptionResults(String[] results) {
         System.out.println("Printing results of encryption");
+        String output = Arrays.toString(results).replace("[","").replace("]","");
         System.out.println(Arrays.toString(results));
+        try {
+            PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+            writer.println(output);
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            System.out.println("Cannot print output");
+        }
     }
 
-    // private static void outputDecryptionResults() {
+     //private static void outputDecryptionResults() {
 
     // }
 }
---
+++ Tester.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ * 
+ * 
+ */
+import java.util.*;
+import java.io.*;
+
+public class Tester {
+
+    private static cryptMode modeOfOperation;
+
+
+    public static void main(String args[]) {
+        System.out.println("");
+        // Generate a plaintext and a key
+        runTest(128,128);
+
+    }
+
+  
+
+    public static boolean runTest(int keyLength, int plaintextLength) {
+      
+        try {
+            String plaintext = generateBitString(plaintextLength);
+            String key = generateBitString(keyLength);
+            // Encrypt the plaintext
+            modeOfOperation = cryptMode.ENCRYPT;
+            Application.testLoadData(plaintext,key);
+            String[] encryptionResults = Application.processEncryption();
+            System.out.println(Arrays.toString(encryptionResults));
+
+            String cipherText = encryptionResults[0];
+            // Now decrypt the plaintext
+            modeOfOperation = cryptMode.DECRYPT;
+            Application.testLoadData(cipherText,key);
+            String[] decryptionResults = Application.processDecryption();
+            System.out.println(Arrays.toString(decryptionResults));
+
+            System.out.println("\n \n \n \n ======================= TEST RESULTS ======================= \n");
+            if (encryptionResults[0] == decryptionResults[0]) {
+                System.out.println("PASS");
+                return true;
+            } else {
+                System.out.println("FAIL");
+                System.err.println("Plaintext: " + plaintext  + " \nKey:  " + key);
+                System.err.println( encryptionResults[0] + " != " + decryptionResults[0] );
+                return false;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return false;
+      
+    }
+
+      /*
+    Helper function used to generate a bit string for plaintext and keys
+    */
+    private static String generateBitString(int length) {
+        String output = "";
+        char[] characters = {'0','1'};
+        Random r = new Random();
+        for (int i = 0; i < length; i++) {
+            int x = r.nextInt(2);
+            output += characters[x];
+        }
+        return output;
+    }
+
+}
